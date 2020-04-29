c2cb50e5fb79b2a007c33ac70e57a9c632e89321
Kevin Tinn <kevin.tinn@wwt.com>
2020-04-29 11:25:05 -0600

    more refactors

--- iac/DataScienceStack.cs
+++ iac/DataScienceStack.cs
@@ -6,29 +6,29 @@ namespace AwsMeetupGroup.DataServices.Infrastructure {
     {
         public DataScienceStack()
         {
-            var sensorTopicName = "aws-meetup-group.iot.sensor-readings.incoming";
-            var iotSensorIngestStream = Kinesis.CreateStream(sensorTopicName);
-            // var iotCert = IoT.createIoT(iotSensorIngestStream.Name);
+            //Create Buckets (Need to manually upload ref .CSV files)
             var sensorRawDataBucket = S3.CreateS3Bucket($"{Common.appName}-sensor-raw-data");
+            var sensorRefDataBucket = S3.CreateS3Bucket($"{Common.appName}-sensor-ref-data");
             var temperatureEnrichedDataBucket = S3.CreateS3Bucket($"{Common.appName}-temp-enriched-data");
             var pressureEnrichedDataBucket = S3.CreateS3Bucket($"{Common.appName}-pressure-enriched-data");
-            
-            var pressureRefFileName = "altitude.csv";
-            var pressureRefDataBucket = S3.CreateS3Bucket($"{Common.appName}-pressure-ref-data");
-            
-            var temperatureRefFileName = "weather.csv";
-            var temperatureRefDataBucket = S3.CreateS3Bucket($"{Common.appName}-temperature-ref-data");
-            
+            var ahtenaResultBucket = S3.CreateS3Bucket($"{Common.appName}-athena-result");
 
+            //Create Incoming Kinesis Data Strean
+            var sensorTopicName = "aws-meetup-group.iot.sensor-readings.incoming";
+            var iotSensorIngestStream = Kinesis.CreateStream(sensorTopicName);
+            
+            
+            //Create Firehose Delivery Streams
             var firehoseRole = Iam.CreateFirehoseRole();
             var sensorS3Firehose = Kinesis.CreateRawDataS3Firehose($"{Common.appName}_sensor_producer_s3", iotSensorIngestStream.Arn, sensorRawDataBucket.Arn, firehoseRole.Arn);
             var temperatureS3Firehose = Kinesis.CreateEnrichedDataS3Firehose($"{Common.appName}_temperature_producer_s3", temperatureEnrichedDataBucket.Arn, firehoseRole.Arn);
             var pressureS3Firehose = Kinesis.CreateEnrichedDataS3Firehose($"{Common.appName}_pressure_producer_s3", pressureEnrichedDataBucket.Arn, firehoseRole.Arn);
 
+            //Create Kinesis Analytics Apps
             var inputStreamColumns = new List<string>(){ "SITE_ID", "SENSOR_TYPE", "SENSOR_READING_VALUE", "READING_TIMESTAMP" };
             var analyticsAppRole = Iam.CreateKinesisAnalyticsAppRole();
             var tempAnalyticsApp = Kinesis.CreateAnalyticsApplication($"{Common.appName}_temperature_data_analytics", new AnalyticsAppS3EnrichmentArgs (
-                temperatureRefDataBucket.Arn,
+                sensorRefDataBucket.Arn,
                 AnalyticsSqlQueries.temperatureQuery,
                 iotSensorIngestStream.Arn,
                 temperatureS3Firehose.Arn,
@@ -36,11 +36,11 @@ namespace AwsMeetupGroup.DataServices.Infrastructure {
                 "temperature",
                 inputStreamColumns,
                 "JSON",
-                temperatureRefFileName,
+                "weather.csv",
                 new List<string>(){"SITE_ID", "OUTSIDE_TEMPERATURE"}
             ));
             var pressureAnalyticsApp = Kinesis.CreateAnalyticsApplication($"{Common.appName}_pressure_data_analytics", new AnalyticsAppS3EnrichmentArgs (
-                pressureRefDataBucket.Arn,
+                sensorRefDataBucket.Arn,
                 AnalyticsSqlQueries.pressureQuery,
                 iotSensorIngestStream.Arn,
                 pressureS3Firehose.Arn,
@@ -48,22 +48,28 @@ namespace AwsMeetupGroup.DataServices.Infrastructure {
                 "pressure",
                 inputStreamColumns,
                 "JSON",
-                pressureRefFileName,
+                "altitude.csv",
                 new List<string>(){"SITE_ID", "ALTITUDE"}
             ));
 
-            var ahtenaResultBucket = S3.CreateS3Bucket($"{Common.appName}-athena-result");
+            //Create Ahtena Database (Need to manually create tables)
             var s3AthenaDb = Athena.CreateDatabase($"{Common.appName}_sensor_athena_db", ahtenaResultBucket.BucketName);
-            this.TopicName = iotSensorIngestStream.Name;
+            this.StreamName = iotSensorIngestStream.Name;
             this.EnrichedPressureBucketName = pressureEnrichedDataBucket.BucketName;
             this.EnrichedTemperatureBucketName = temperatureEnrichedDataBucket.BucketName;
+            this.RawSensorDataBucketName = sensorRawDataBucket.BucketName;
+            this.ReferenceDataBucket = sensorRefDataBucket.BucketName;
         }
 
         [Output]
-        public Output<string> TopicName { get; set; }
+        public Output<string> StreamName { get; set; }
         [Output]
         public Output<string> EnrichedPressureBucketName { get; set; }
         [Output]
         public Output<string> EnrichedTemperatureBucketName { get; set; }
+
+        public Output<string> RawSensorDataBucketName { get; set; }
+        [Output]
+        public Output<string> ReferenceDataBucket { get; set; }
     }
 }
--- kinesis-producer/produce.py
+++ kinesis-producer/produce.py
@@ -1,16 +1,18 @@
 import boto3
 import random
+import os
 
 client = boto3.client('kinesis', 'us-west-2')
-
+streamName = os.getenv('STREAM_NAME')#'aws-meetup-group.iot.sensor-readings.incoming-a1aa62d'
+print('Stream name:' + streamName)
 
 my_shard_id = ""
 for x in range(100):
 	siteId = random.randint(1,11)
 	sensorType = "TEMPERATURE" if x % 2 == 0 else "PRESSURE"
-	message = '{"SITE_ID":"' + str(siteId) + '","SENSOR_TYPE":"' + sensorType + '","SENSOR_READING_VALUE":"' + random.uniform(50.0,212.0) + '","READING_TIMESTAMP":"04/28/2020 17:52:16"}'
+	message = '{"SITE_ID":"' + str(siteId) + '","SENSOR_TYPE":"' + sensorType + '","SENSOR_READING_VALUE":"' + str(random.uniform(50.0,212.0)) + '","READING_TIMESTAMP":"04/28/2020 17:52:16"}'
 	response = client.put_record (
-	    StreamName='aws-meetup-group.iot.sensor-readings.incoming-041aac9',
+	    StreamName=streamName,
 	    Data=message.encode(),
 	    PartitionKey="1"
 	)
@@ -22,7 +24,7 @@ for x in range(100):
 
 my_shard_id = response['ShardId']
 
-shard_iterator = client.get_shard_iterator(StreamName='aws-meetup-group.iot.sensor-readings.incoming-2be9e18',
+shard_iterator = client.get_shard_iterator(StreamName=streamName,
                                                       ShardId=my_shard_id,
                                                       ShardIteratorType='TRIM_HORIZON')
 
